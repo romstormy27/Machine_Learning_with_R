33cea49eee03260289873302e6a00751aee04b2a
fahrury-rdd27 <fahrury_rdd27@apps.ipb.ac.id>
2021-01-01 10:43:21 +0700

    add loan proposal predictor with Decision Tree learner

--- Loan_Proposal_Predictor.R
+++ Loan_Proposal_Predictor.R
@@ -42,3 +42,25 @@ library(C50)
 credit_model <- C5.0(credit_train[-17], credit_train$default)
 credit_model
 summary(credit_model)
+
+#----- Testing and evaluating the model -----
+credit_pred <- predict(credit_model, credit_test)
+
+library(gmodels)
+CrossTable(credit_test$default, credit_pred, prop.chisq = F, prop.c = F, prop.r = F, dnn = c('actual default','predicted default'))
+
+#----- Improving model with boosting method (random forest analogous) -----
+credit_boost10 <- C5.0(credit_train[-17], credit_train$default, trials = 10)
+credit_boost10
+summary(credit_boost10)
+
+credit_boost10_pred <- predict(credit_boost10, credit_test)
+CrossTable(credit_test$default, credit_boost10_pred, prop.chisq = F, prop.c = F, prop.r = F, dnn = c('actual default','predicted default'))
+
+#adding cost matrix for the error
+error_cost <- matrix(c(0, 1, 4, 0), nrow = 2)
+error_cost
+
+credit_cost <- C5.0(credit_train[-17], credit_train$default, costs = error_cost)
+credit_cost_pred <- predict(credit_cost, credit_test)
+CrossTable(credit_test$default, credit_cost_pred, prop.chisq = F, prop.c = F, prop.r = F, dnn = c('actual default','predicted default'))
